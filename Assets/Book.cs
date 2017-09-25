2a04db62c6ee8c55c6eceb55db7801f6fae86625
Stephen McLeod <smcl773@aucklanduni.ac.nz>
2017-09-25 13:03:31 +1300

    added billboarding

---
+++ Assets/Book.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding.Serialization.JsonFx;
+
+public class Book : MonoBehaviour {
+    //link to BookTable in Azure easytables
+    public string _WebsiteURL = "http://holobooks.azurewebsites.net/tables/books?zumo-api-version=2.0.0";
+    public UnityEngine.GameObject[] books;
+    public GameObject myPrefab;
+    // Use this for initialization
+    void Start () {
+        //Reguest.GET makes a JSON string
+        string jsonResponse = Request.GET(_WebsiteURL);
+        //Just in case something went wrong with the request we check the reponse and exit if there is no response.
+        if (string.IsNullOrEmpty(jsonResponse)) { return; }
+        //creates an array of the table in C#
+        BookDetails[] Details = JsonReader.Deserialize<BookDetails[]>(jsonResponse);
+        //creates a new book
+        var newBook = (GameObject)Instantiate(myPrefab);
+        //sets the text for title to the title in the table
+        newBook.transform.Find("Title").GetComponent<TextMesh>().text = Details[0].Title;
+        //sets the text for the author to the author in the table
+        newBook.transform.Find("Author").GetComponent<TextMesh>().text = Details[0].Author;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/BookTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//creates a table of books
+class BookDetails
+{
+    //List of the headers/fields in the table
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public string Height { get; set; }
+    public string Publisher { get; set; }
+    
+}
